3270c13276c315d823368a2a7874c12c98b41093
Gulburak1 <116088697+Gulburak1@users.noreply.github.com>
2022-12-19 18:06:25 +0600

    Add files via upload

---
+++ inde.js
@@ -0,0 +1,11 @@
+let someDiv = document.getElementById("hw4");
+let JSONobj = fetch("https://jsonplaceholder.typicode.com/posts")
+.then((response) => {
+    const object = response.json();
+    return object
+})
+.then((data)=>{
+    for(key in data) {
+    someDiv.innerHTML += `<div class="posts"><span>userId: ${data[key].userId}</span><span>Id: ${data[key].id}</span><span>title: ${data[key].title}</span><span>body:${data[key].body}</span></div>`
+    } 
+});
